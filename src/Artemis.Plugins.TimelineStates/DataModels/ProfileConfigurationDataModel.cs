9fbba14b0a110c4591f836f604a6504160b50a95
mrtnRitter <mrtn.ritter@googlemail.com>
2025-02-13 22:05:22 +0100

    ctie

--- src/Artemis.Plugins.TimelineStates/DataModels/ProfileConfigurationDataModel.cs
+++ src/Artemis.Plugins.TimelineStates/DataModels/ProfileConfigurationDataModel.cs
@@ -3,16 +3,13 @@ using Artemis.Core.Modules;
 
 namespace Artemis.Plugins.TimelineStates.DataModels;
 
-public class ProfileConfigurationDataModel : DataModel
+public class ProfileConfigurationDataModel(ProfileConfiguration profileConfiguration) : DataModel
 {
-    private readonly ProfileConfiguration _profileConfiguration;
-
-    public ProfileConfigurationDataModel(ProfileConfiguration profileConfiguration)
-    {
-        _profileConfiguration = profileConfiguration;
-    }
+    private readonly ProfileConfiguration _profileConfiguration = profileConfiguration;
 
     public bool IsLoaded => _profileConfiguration.Profile != null;
     public bool IsSuspended => _profileConfiguration.IsSuspended;
     public bool ActivationConditionMet => _profileConfiguration.ActivationConditionMet;
-}
+    public string Profilename => _profileConfiguration.Profile.Name;
+
+};
