c5ddcb948e9a1a974e6636435c6fcb8b3d2efb3c
mrtnRitter <mrtn.ritter@googlemail.com>
2025-02-20 18:54:25 +0100

    cicite

--- src/Artemis.Plugins.TimelineStates/DataModels/TimelineDataModel.cs
+++ src/Artemis.Plugins.TimelineStates/DataModels/TimelineDataModel.cs
@@ -12,8 +12,9 @@ public class TimelineDataModel : DataModel
     {
         _layer = layer;
 
-        AddDynamicChild(layer.Timeline.Length.GetHashCode().ToString(), layer.Timeline.Length, "Total Length");
-        AddDynamicChild(layer.Timeline.StartSegmentLength.GetHashCode().ToString(), layer.Timeline.StartSegmentLength.Milliseconds, "Start Segment Length");
+        AddDynamicChild(layer.Timeline.Length.GetHashCode().ToString(), layer.Timeline.Length.TotalMilliseconds, "Total Length");
+        AddDynamicChild(layer.Timeline.StartSegmentLength.GetHashCode().ToString(), layer.Timeline.StartSegmentLength.TotalMilliseconds, "Start Segment Length");
+        AddDynamicChild(layer.Timeline.Position.GetHashCode().ToString(), layer.Timeline.Position.TotalMilliseconds, "Current Playhead Position");
 
     }
 
